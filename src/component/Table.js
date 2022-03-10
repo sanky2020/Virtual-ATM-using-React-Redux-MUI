027968c9cf8b93146a8be9e87947fc05841e9f74
sanky2020 <sankyk2020@gmail.com>
2022-03-10 15:14:22 +0530

    Update Table.js

--- src/component/Table.js
+++ src/component/Table.js
@@ -8,52 +8,37 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import { Typography } from '@mui/material';
-import { useSelector } from 'react-redux';
 
 const columns = [
   { id: 'date', label: 'Date', minWidth: 50 },
   { id: 'time', label: 'Time', minWidth: 50 },
   {
-    id: 'type',
+    id: 'accType',
     label: 'Action Type',
     minWidth: 50,
     align: 'right',
-    // format: (value) => value.toLocaleString('en-US'),
   },
   {
     id: 'amount',
     label: 'Amount',
     minWidth: 50,
     align: 'right',
-    // format: (value) => value.toLocaleString('en-US'),
   },
   {
     id: 'balance',
-    label: 'Balance',
+    label: 'Current-Balance',
     minWidth: 50,
     align: 'right',
-    // format: (value) => value.toFixed(2),
   },
 ];
 
+export default function StickyHeadTable(props) {
+  
+  let rows=props.rows;
 
-
-export default function StickyHeadTable() {
-    function createData(date, time, type, amount, balance) {
-   
-        return {date, time, type, amount, balance };
-      }
-      const { date, time, accType, amount, balance } = useSelector(
-        (state) => state.amountReducer
-      );
-    const rows = [
-        createData(date , time, accType, amount, balance),
-      
-    ];
-    
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
-
+    
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -64,7 +49,7 @@ export default function StickyHeadTable() {
   };
 
   return (
-    <Paper sx={{ width: '80%', overflow: 'hidden', margin:"0 auto" }}>
+    <Paper sx={{ width: '80%', overflow: 'hidden', margin:"1rem auto" }}>
     <Typography variant='h6'>Statement:</Typography>
       <TableContainer sx={{ maxHeight: 400,width:'98%' }}>
         <Table stickyHeader aria-label="sticky table" style={{width:'98%',border: '1px solid #E7EBF0' }}>
@@ -75,7 +60,6 @@ export default function StickyHeadTable() {
                 style={{border: '1px solid #E7EBF0'}}
                   key={column.id}
                   align={column.align}
-                //   style={{ minWidth: column.minWidth }}
                 >
                   {column.label}
                 </TableCell>
@@ -98,16 +82,7 @@ export default function StickyHeadTable() {
                         </TableCell>
                       );
                     })}
-                    {/* {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })} */}
+                    
                   </TableRow>
                 );
               })}
@@ -115,7 +90,7 @@ export default function StickyHeadTable() {
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[5 , 10 , 15]}
+        rowsPerPageOptions={[5 , 10 , 15, 20]}
         component="div"
         count={rows.length}
         rowsPerPage={rowsPerPage}
