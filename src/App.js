adb2cd4054b2f6b48088a4bc5e9d3574b475afad
sanky2020 <sankyk2020@gmail.com>
2022-03-10 15:12:40 +0530

    added condition to display/hide welcome component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Bank from "./component/Bank";
 import "../src/App.css";
 import { useSelector } from "react-redux";
 import Statement from "./component/Statement";
+import Welcome from "./component/Welcome";
 
 function App() {
   const balance = useSelector((state) => state.amountReducer.balance);
   const amount = useSelector((state) => state.amountReducer.amount);
+  const [welcome, setWelcome] = useState(true);
 
-  
-  
+  const callback=(data)=>{
+    console.log(data)
+    setWelcome(false)
+  }
   return (
     <div className="App">
+    {welcome ? <Welcome callback={callback}/> : ""}
      
       <h1>Account Balance: {balance} INR</h1>
 
